8438196a69493216eccca252db9d9553877b717d
AyushAhuja2707 <ayushahuja200@gmail.com>
2024-12-15 14:34:15 +0530

    Update Cors Policy 1

--- src/main/java/com/ayush/estore/AyushStore/config/SwaggerConfig.java
+++ src/main/java/com/ayush/estore/AyushStore/config/SwaggerConfig.java
@@ -13,33 +13,33 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 @Configuration
 public class SwaggerConfig {
 
-    // @Bean
-    // public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
-    // return new io.swagger.v3.oas.models.OpenAPI()
-    // .info(new Info()
-    // .title("AyushStore API")
-    // .version("1.0.0")
-    // .description("Documentation for AyushStore APIs"));
-    // }
+        // @Bean
+        // public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
+        // return new io.swagger.v3.oas.models.OpenAPI()
+        // .info(new Info()
+        // .title("AyushStore API")
+        // .version("1.0.0")
+        // .description("Documentation for AyushStore APIs"));
+        // }
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        // Define the security scheme
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
+        @Bean
+        public OpenAPI customOpenAPI() {
+                // Define the security scheme
+                SecurityScheme securityScheme = new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT");
 
-        // Apply the security scheme globally
-        SecurityRequirement securityRequirement = new SecurityRequirement()
-                .addList("JWT");
+                // Apply the security scheme globally
+                SecurityRequirement securityRequirement = new SecurityRequirement()
+                                .addList("JWT");
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("AyushStore API")
-                        .version("1.0.0")
-                        .description("Documentation for AyushStore APIs"))
-                .addSecurityItem(securityRequirement)
-                .schemaRequirement("JWT", securityScheme);
-    }
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("AyushStore's API")
+                                                .version("1.0.0")
+                                                .description("Documentation for AyushStore APIs"))
+                                .addSecurityItem(securityRequirement)
+                                .schemaRequirement("JWT", securityScheme);
+        }
 }
